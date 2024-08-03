58b1b22e3b10b76be105cbb91afac527748db833
Alexander Eckert <eckertalex@proton.me>
2024-08-03 22:46:19 +0200

    add auto-dark-mode to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -305,6 +305,23 @@ require("lazy").setup({
     },
 
     {
+        "f-person/auto-dark-mode.nvim",
+        config = function()
+            require("auto-dark-mode").setup({
+                update_interval = 1000,
+                set_dark_mode = function()
+                    vim.api.nvim_set_option_value("background", "dark", {})
+                    vim.cmd("colorscheme rose-pine")
+                end,
+                set_light_mode = function()
+                    vim.api.nvim_set_option_value("background", "light", {})
+                    vim.cmd("colorscheme rose-pine")
+                end,
+            })
+        end,
+    },
+
+    {
         "fladson/vim-kitty",
     },
 
