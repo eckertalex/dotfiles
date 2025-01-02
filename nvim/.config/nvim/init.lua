45a6314960f14cf5a6feabdbc7b738e085c3bd24
Alexander Eckert <eckertalex@proton.me>
2025-01-02 22:25:47 +0100

    use ts-comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -415,26 +415,9 @@ require("lazy").setup({
     },
 
     {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        config = function()
-            require("ts_context_commentstring").setup({
-                enable_autocmd = false,
-            })
-        end,
-    },
-
-    {
-        "echasnovski/mini.comment",
-        config = function()
-            require("mini.comment").setup({
-                options = {
-                    custom_commentstring = function()
-                        return require("ts_context_commentstring.internal").calculate_commentstring()
-                            or vim.bo.commentstring
-                    end,
-                },
-            })
-        end,
+        "folke/ts-comments.nvim",
+        event = "VeryLazy",
+        opts = {},
     },
 
     {
