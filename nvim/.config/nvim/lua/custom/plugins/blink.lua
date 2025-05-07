433c2dcd5c2e90bd6278b1d2f9e43d4100b08188
Alexander Eckert <eckertalex@proton.me>
2025-05-07 21:18:58 +0200

    update blink

--- nvim/.config/nvim/lua/custom/plugins/blink.lua
+++ nvim/.config/nvim/lua/custom/plugins/blink.lua
@@ -2,20 +2,18 @@ return {
 	{
 		"saghen/blink.cmp",
 		dependencies = "rafamadriz/friendly-snippets",
+		version = "1.*",
 		event = "InsertEnter",
 		opts = {
-			fuzzy = {
-				implementation = "lua",
-			},
 			keymap = { preset = "default" },
 			appearance = {
-				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
 			},
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
 			},
 			signature = { enabled = true },
+			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extend = { "sources.default" },
 	},
