186805c9ba5cbfab7d846332af7813ac0f99bb78
Alexander Eckert <eckertalex@proton.me>
2025-07-12 10:28:40 +0200

    update deps

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -74,6 +74,7 @@ return {
 			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
 			{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
 			{ "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+			{ "<leader>sm", function() require('custom.config.multigrep').multigrep() end, desc = "Multigrep" },
 			{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
 			-- search
 			{ "<leader>sn", function() Snacks.picker.notifications() end, desc = "Notification History" },
