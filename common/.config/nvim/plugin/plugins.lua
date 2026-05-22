050a45b611bcd48d0a59278c03faf7fa5fc4ee2e
Alexander Eckert <eckertalex@proton.me>
2026-05-22 22:06:17 +0200

    remove claudecode.nvim

--- common/.config/nvim/plugin/plugins.lua
+++ common/.config/nvim/plugin/plugins.lua
@@ -61,36 +61,3 @@ vim.pack.add({ "https://github.com/MagicDuck/grug-far.nvim" })
 vim.keymap.set({ "n", "x" }, "<leader>si", function()
     require("grug-far").open({ visualSelectionUsage = "auto-detect" })
 end, { desc = "grug-far: Search within range" })
-
--- Deferred to VimEnter: claudecode pulls in ~20 modules at require time
--- (~14ms cold). Nothing in it is needed until the user invokes <leader>a*,
--- so set it up after Neovim has finished starting. See `:h VimEnter`.
-vim.api.nvim_create_autocmd("VimEnter", {
-    group = augroup,
-    once = true,
-    desc = "Set up claudecode after startup",
-    callback = function()
-        vim.pack.add({ "https://github.com/coder/claudecode.nvim" })
-
-        require("claudecode").setup()
-
-        vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<cr>", { desc = "Toggle claude" })
-        vim.keymap.set("n", "<leader>af", "<cmd>ClaudeCodeFocus<cr>", { desc = "Focus claude" })
-        vim.keymap.set("n", "<leader>ar", "<cmd>ClaudeCode --resume<cr>", { desc = "Resume claude" })
-        vim.keymap.set("n", "<leader>aC", "<cmd>ClaudeCode --continue<cr>", { desc = "Continue claude" })
-        vim.keymap.set("n", "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", { desc = "Add current buffer" })
-        vim.keymap.set("v", "<leader>as", "<cmd>ClaudeCodeSend<cr>", { desc = "Send to claude" })
-        vim.keymap.set("n", "<leader>aa", "<cmd>ClaudeCodeAccept<cr>", { desc = "Accept diff" })
-        vim.keymap.set("n", "<leader>ad", "<cmd>ClaudeCodeDeny<cr>", { desc = "Deny diff" })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            group = augroup,
-            pattern = { "oil", "minifiles", "netrw" },
-            callback = function()
-                vim.keymap.set("n", "<leader>as", "<cmd>ClaudeCodeTreeAdd<cr>", {
-                    desc = "Add to claude",
-                })
-            end,
-        })
-    end,
-})
